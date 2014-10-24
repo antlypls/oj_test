88167d89cff569e3c7b7a0d3b3d90d51a75280ac
Anatoliy Plastinin <anatoliy.plastinin@gmail.com>
2014-10-24 22:57:18 +0400

    Add blog model + specs configuration

---
+++ spec/controllers/blogs_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe BlogsController do
+  context 'JSON request' do
+    describe 'GET index' do
+      let!(:blog) { create(:blog) }
+      subject { get :index, format: :json }
+
+      it 'returns ok' do
+        is_expected.to be_success
+      end
+
+      it 'returns valid JSON' do
+        result = JSON.parse(subject.body)
+
+        expect(result).to be_an(Array)
+        expect(result.first['title']).to eq(blog.title)
+      end
+    end
+  end
+end
